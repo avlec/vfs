4a10a595d8ce6a2787047f76df2419fd1d2c0fba
Alec <avlecxk@gmail.com>
2019-04-10 14:22:26 -0700

    Implemented directory functionality - open - constructing page map for first 10 pages.

--- file/file.c
+++ file/file.c
@@ -22,9 +22,56 @@ struct directory {
 };
 typedef struct directory * directory_t;
 
+uint16_t * build_page_map(vfs_t vfs, inode_t inode)
+{
+    // count full pages, and add an extra page for an unfilled page.
+    uint32_t page_count = inode->file_size / 512 + ((inode->file_size % 512 == 0) ? 0 : 1);
+    uint16_t * page_map = calloc(page_count, sizeof(int16_t));
+
+    printf("page map for %d pages\r\n", page_count);
+
+    for(int i = 0; i < ((page_count < 10) ? page_count : 10); ++i)
+    {
+        page_map[i] = inode->d_pages[i];
+    }
+
+    if (page_count > 10)
+    {
+        // TODO add all single indirect pages and double indirect pages
+    }
+    return page_map;
+}
+
 directory_t directory_open(vfs_t vfs, char * path)
 {
+    directory_t dir = (directory_t) malloc(sizeof(struct directory));
+    dir->vfs = vfs;
+    dir->path = (char *) calloc(strlen(path) + 1, sizeof(char));
+    memcpy(dir->path, path, strlen(path));
+    dir->name = (char *) calloc(31, sizeof(char));
 
+    char * token = strtok(path, "/");
+    uint16_t current_inode = 0;
+    inode_t current_dir = vfs_get_inode(vfs, current_inode);
+    uint16_t * current_dir_page_map = build_page_map(vfs, current_dir);
+    while(token != NULL)
+    {
+        memset(dir->name, 0, 31);
+        memcpy(dir->name, token, strlen(token));
+
+        // search in the pages of current_dir for
+        // the entry matching token
+        // if found update current dir
+        free(current_dir_page_map);
+        current_dir_page_map = build_page_map(vfs, current_dir);
+
+        token = strtok(NULL, "/");
+    }
+    free(current_dir_page_map);
+
+    dir->inode_number = current_inode;
+    dir->inode = current_dir;
+    return dir;
 }
 
 void directory_close(directory_t dir)
@@ -42,13 +89,12 @@ file_t file_create(vfs_t vfs, char * file_path)
     file_t new_file = (file_t) malloc(sizeof(struct file));
     new_file->vfs = vfs;
     // create and store new inode
-    //new_file->inode_number =  vfs_new_file_inode(vfs);
+    new_file->inode_number =  vfs_new_file_inode(vfs);
     // get inode
-    //new_file->inode = vfs_get_inode(vfs, new_file->inode_number);
+    new_file->inode = vfs_get_inode(vfs, new_file->inode_number);
 
     // parse filepath
     // build directory path
-    // (/.*/)*[^/]*
     // get filename
     size_t string_length = strlen(file_path) + 1;
     new_file->path = (char *) malloc(sizeof(char) * string_length);
@@ -76,8 +122,6 @@ file_t file_create(vfs_t vfs, char * file_path)
         }
     }
 
-    printf("last slash %d, strlen %d\r\n", last_slash, string_length);
-
     memcpy(directory_path, file_path, last_slash);
     memcpy(new_file->name, file_path+last_slash, string_length - last_slash);
 
@@ -100,7 +144,7 @@ void file_open(vfs_t vfs, char * filepath)
 int main() {
     vfs_t vfs = vfs_open("vdisk.img");
 
-    file_t file = file_create(vfs, "/home");
+    file_t file = file_create(vfs, "/avlec");
 
     return EXIT_SUCCESS;
 }
